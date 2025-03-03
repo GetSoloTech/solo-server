17dae670f34644cca877ad29643516c73661f731
ddiddi <dhruv.diddi+1@gmail.com>
2025-03-03 00:22:33 -0800

    chore: update version for server

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="solo-server",
-    version="0.3.6",
+    version="0.3.7",
     author="Dhruv Diddi",
     author_email="dhruv.diddi@gmail.com",
     description="AIOps for the Physical World.",
