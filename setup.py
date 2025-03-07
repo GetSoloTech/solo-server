eb8ffd82119024ab82d1b934fd23b1d4028a7914
ddiddi <dhruv.diddi+1@gmail.com>
2025-03-07 13:09:23 -0800

    feat: add @@ answers with core

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="solo-server",
-    version="0.3.10",
+    version="0.4.0",
     author="Dhruv Diddi",
     author_email="dhruv.diddi@gmail.com",
     description="Platform for Hardware Aware Inference.",
