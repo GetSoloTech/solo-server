a13cfa4f828c9ee06e687358e0b831fe84c2bfd6
ddiddi <dhruv.diddi+1@gmail.com>
2025-03-26 09:22:32 -0700

    feat: add containers with litserve servers default

---
+++ solo_server/examples/merged/deepseek-r1-distill-qwen-1.5b/client.py
@@ -0,0 +1,15 @@
+import requests
+
+def test_server(text):
+    # API endpoint URL
+    url = "http://127.0.0.1:8000/predict"
+    # Request payload
+    payload = {"text": text}
+    # POST request to the server
+    response = requests.post(url, json=payload)
+    # Print the response from the server
+    print(response.json())
+
+if __name__ == "__main__":
+    sample_text = "I love machine learning. My experience with LitServe has been amazing!"
+    test_server(sample_text)
