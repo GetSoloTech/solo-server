f451d565bfd38c8db998ff15f9fec610d6c0ba62
ddiddi <dhruv.diddi+1@gmail.com>
2025-04-27 23:08:23 -0700

    patch: update llamacpp for 0.4.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="solo-server",
-    version="0.4.2",
+    version="0.4.3",
     author="Dhruv Diddi",
     author_email="dhruv.diddi@gmail.com",
     description="Platform for Hardware Aware Inference.",
